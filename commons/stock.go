856e838500b28a8884d3edf3609f56193f584943
Helloworld Park <helloworld.park@gmail.com>
2019-04-05 15:58:43 +0900

    #2 코스닥 코스피 구분도 필요해서 추가

--- commons/stock.go
+++ commons/stock.go
@@ -1,7 +1,18 @@
 package commons
 
+// Market is an enum type representing the type of the stock market
+type Market string
+
+const (
+	// KOSPI market
+	KOSPI = "kospi"
+	// KOSDAC market
+	KOSDAC = "kosdac"
+)
+
 // Stock is a struct describing each stock item
 type Stock struct {
-	Name    string
-	StockID string
+	Name       string
+	StockID    string
+	MarketType Market
 }
