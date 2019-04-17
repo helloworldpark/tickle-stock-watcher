ee2f5898b68bbdc054400f650015d7d0656a5bc6
Helloworld Park <helloworld.park@gmail.com>
2019-04-17 17:14:46 +0900

    #28 잠시 빌드스크립트 수정

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ git pull origin dev
 # Build
 go build
 # Run Cloud Proxy
-./cloud_sql_proxy -instances=ticklemeta-203110:asia-east1:ticklemeta-coininfodb=tcp:3306 &
+#./cloud_sql_proxy -instances=ticklemeta-203110:asia-east1:ticklemeta-coininfodb=tcp:3306 &
 # Run
-pmgo start ./tickle-stock-watcher # --args="-credential" --args="./credee.json"
+pmgo start ./ tickle-stock-watcher # --args="-credential" --args="./credee.json"
