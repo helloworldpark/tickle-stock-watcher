f127efd681262ca2623ed3930a25c56d391f42c1
Helloworld Park <helloworld.park@gmail.com>
2019-05-05 08:59:51 +0900

    run.sh 업데이트

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Pull from git
-git pull origin dev
-# Build
-go build
+git pull origin master
 # Run Cloud Proxy
 #./cloud_sql_proxy -instances=ticklemeta-203110:asia-east1:ticklemeta-coininfodb=tcp:3306 &
 # Run
-pmgo start ./ tickle-stock-watcher # --args="-credential" --args="./credee.json"
+pmgo delete tickle-stock-watcher
+pmgo start $(pwd)/ tickle-stock-watcher --args="-credential" --args="$(pwd)/credee.json" --args="-telegram" --args="$(pwd)/telegrambotconfig.json"
