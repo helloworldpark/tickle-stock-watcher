7f717c5823bb2d2a58b1d3f8816f9c594b43f841
Helloworld Park <helloworld.park@gmail.com>
2019-11-17 08:01:07 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # Pull from git
 git pull origin master
+# Build
+go build
+echo "[TickleStockWatcher] Build Complete"
 # Run Cloud Proxy
 #./cloud_sql_proxy -instances=ticklemeta-203110:asia-east1:ticklemeta-coininfodb=tcp:3306 &
+echo "[TickleStockWatcher] Cloud SQL Proxy On"
 # Run
-pmgo delete tickle-stock-watcher
-pmgo start $(pwd)/ tickle-stock-watcher --args="-credential" --args="$(pwd)/credee.json" --args="-telegram" --args="$(pwd)/telegrambotconfig.json"
+LISTS=$(pmgo list)
+if [[ $LISTS == *"tickle-stock-watcher"* ]]; then
+	pmgo delete tickle-stock-watcher
+fi
+pmgo start ./ tickle-stock-watcher --args="-credential" --args="$(pwd)/credee.json" --args="-telegram" --args="$(pwd)/telegrambotconfig.json"
