8c6b606c371f70da56acc96cbfe11d8c389ab263
Helloworld Park <helloworld.park@gmail.com>
2020-01-24 18:00:19 +0900

    [Push] 텔레그램의 글자수 제한을 우회

--- push/pushmanager.go
+++ push/pushmanager.go
@@ -1,6 +1,10 @@
 package push
 
-import "github.com/helloworldpark/tickle-stock-watcher/commons"
+import (
+	"strings"
+
+	"github.com/helloworldpark/tickle-stock-watcher/commons"
+)
 
 type msgTask = func()
 
@@ -8,6 +12,8 @@ type msgTask = func()
 type Manager struct {
 	tasksTelegram chan msgTask
 }
+
+const msgMaxLength = 4096
 
 // NewManager creates an initialized Push Manager
 func NewManager() *Manager {
@@ -25,7 +31,40 @@ func NewManager() *Manager {
 
 // PushMessage pushes message to Telegram Bot
 func (m *Manager) PushMessage(msg string, userid int64) {
+	if len(msg) == 0 {
+		return
+	}
+
 	m.tasksTelegram <- func() {
-		SendMessageTelegram(userid, msg)
+		if len(msg) < msgMaxLength {
+			SendMessageTelegram(userid, msg)
+			return
+		}
+
+		splits := strings.Split(msg, "\n")
+		if len(splits) == 1 {
+			last := 0
+			for last < len(msg) {
+				SendMessageTelegram(userid, msg[last:commons.MinInt(last+msgMaxLength, len(msg))])
+				last += msgMaxLength
+			}
+		} else {
+			var tmpMsg string
+			for _, line := range splits {
+				if len(tmpMsg)+len(line) <= msgMaxLength {
+					tmpMsg += (line + "\n")
+				} else {
+					SendMessageTelegram(userid, tmpMsg)
+					if len(line) > 0 && line[0] == ' ' {
+						tmpMsg = ("*" + line[1:] + "\n")
+					} else {
+						tmpMsg = (line + "\n")
+					}
+				}
+			}
+			if len(tmpMsg) > 0 {
+				SendMessageTelegram(userid, tmpMsg)
+			}
+		}
 	}
 }
