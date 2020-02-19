abd01e49e380ea0fa87c9ff5e3e82281e5e52862
Helloworld Park <helloworld.park@gmail.com>
2020-02-19 21:37:27 +0900

    [Test] Drive API: JWT

--- database/jwt.go
+++ database/jwt.go
@@ -1,24 +1,43 @@
 package database
 
 import (
+	"context"
+	"fmt"
 	"io/ioutil"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v2"
 )
 
 func CreateJWTToken(jsonPath string) *oauth2.Token {
+	cred, err := google.FindDefaultCredentials(context.Background(), drive.DriveScope, drive.DriveFileScope)
+	if err != nil {
+		fmt.Errorf("%+v", err.Error())
+	}
 	jsonKey, err := ioutil.ReadFile(jsonPath)
 	if err != nil {
-		panic(err)
+		fmt.Errorf("%+v", err.Error())
 	}
-	tokenSource, err := google.JWTAccessTokenSourceFromJSON(jsonKey, "https://stock.ticklemeta.kr")
-	if err != nil {
-		panic(err)
+
+	var tokenSource oauth2.TokenSource
+
+	if cred != nil {
+		tokenSource = cred.TokenSource
+		fmt.Println("Credential is not nil")
+	} else if len(jsonKey) > 0 {
+		tokenSource, err = google.JWTAccessTokenSourceFromJSON(jsonKey, "https://stock.ticklemeta.kr")
+		fmt.Println("Token Source From JWTAccessTokenSourceFromJSON")
+		if err != nil {
+			fmt.Errorf("%+v", err.Error())
+		}
+	} else {
+		panic("No way")
 	}
+
 	token, err := tokenSource.Token()
 	if err != nil {
-		panic(err)
+		fmt.Errorf("%+v", err.Error())
 	}
 	return token
 }
--- database/jwt_test.go
+++ database/jwt_test.go
@@ -1,7 +1,10 @@
 package database
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"testing"
 )
@@ -13,4 +16,31 @@ func TestJWT(t *testing.T) {
 	fmt.Printf("access token: %s\n", token.AccessToken)
 	fmt.Printf("expires at: %v\n", token.Expiry)
 
+	testDriveAPI(token.AccessToken)
+}
+
+func testDriveAPI(accessToken string) {
+	fmt.Println("Test Drive API")
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://www.googleapis.com/drive/v2/files?access_token=%s", accessToken), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	read, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	var readJSON interface{}
+	json.Unmarshal(read, &readJSON)
+
+	fmt.Println("FILES")
+	fmt.Println(readJSON)
 }
