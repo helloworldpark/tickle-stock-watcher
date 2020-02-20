1755af056356ae7fc11356553bf2e4f3c27b3c18
Helloworld Park <helloworld.park@gmail.com>
2020-02-20 09:19:47 +0900

    [Test] Drive API: JWT

--- database/jwt_test.go
+++ database/jwt_test.go
@@ -8,10 +8,11 @@ import (
 	"testing"
 )
 
+const jsonPath = "/Users/shp/Documents/projects/ticklemeta-203110-709122f3e3af.json"
+
 func TestJWT(t *testing.T) {
 	// envKey := "GOOGLE_APPLICATION_CREDENTIALS"
 	// jsonPath := os.Getenv(envKey)
-	jsonPath := "/Users/shp/Documents/projects/ticklemeta-203110-709122f3e3af.json"
 	token := CreateJWTToken(jsonPath)
 	fmt.Printf("access token: %s\n", token.AccessToken)
 	fmt.Printf("expires at: %v\n", token.Expiry)
@@ -47,15 +48,15 @@ func testDriveAPI(accessToken string) {
 	}
 	if itemsRaw, ok := mapJSON["items"]; ok {
 		if items, ok := itemsRaw.([]interface{}); ok {
-			fmt.Println("FILES: ", len(items))
+			fmt.Println("Items: ", len(items))
 			for _, item := range items {
 				if itemMap, ok := item.(map[string]interface{}); ok {
 					for k, v := range itemMap {
 						fmt.Printf("%s:    %v\n", k, v)
 					}
+					fmt.Println("------------------------")
 				}
 			}
 		}
 	}
-
 }
